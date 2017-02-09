fb4efb231f39f145fd318bcfba5289c9cf1f26f7
Tchoupinax <corentinfiloche@hotmail.fr>
2017-02-09 11:06:31 +0100

    remise en forme du main

--- app/src/main/java/ul/iutmetz/filoche1u/projet_android/MainActivity.java
+++ app/src/main/java/ul/iutmetz/filoche1u/projet_android/MainActivity.java
@@ -5,14 +5,11 @@ import android.net.Uri;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Editable;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
@@ -29,21 +26,32 @@ public class MainActivity extends AppCompatActivity {
     private int position = 0;
     private AlertDialog.Builder alertBox = null;
     private final MainActivity self = this;
-    //
-    //
+    // VIEWS
+    private ListView list_act;
+    private ListView list_sai;
+    // VARIABLES
+    private String[] acteurs= {"Eliza Taylor", "Thomas McDonell", "Marie Avgeropoulos", "Henry Ian Cusick", "Isaiah Washington", "Bob Morley"};
+    private String[] saisons = {"Saison 1", "Saison 2", "Saison 3", "Saison 4"};
     //
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // OVERRIDE
         super.onCreate(savedInstanceState);
+        // AJOUT DE LAYOUT MAIN
         setContentView(R.layout.activity_main);
-
-
-        ListView list_act;
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+        // RECUPERATION DES VIEWS
         list_act = (ListView) findViewById(R.id.liste_acteur);
-        final String[] acteurs = {"Eliza Taylor", "Thomas McDonell", "Marie Avgeropoulos", "Henry Ian Cusick", "Isaiah Washington", "Bob Morley"};
+        list_sai = (ListView) findViewById(R.id.Liste_saison);
+        // CREATION DES ARRAYADAPTER
         final ArrayAdapter<String> adapter_act = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, acteurs);
+        final ArrayAdapter<String> adapter_sai = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, saisons);
+        // AJOUT DES ARRAY AUX VIEWS
         list_act.setAdapter(adapter_act);
-        // Long click listener
+        list_sai.setAdapter(adapter_sai);
+        // LONG CLICK LISTENER
         list_act.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(final AdapterView<?> parent, final View view, int position, long id) {
@@ -66,20 +74,7 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
-
-
-        ListView list_sai;
-        list_sai = (ListView) findViewById(R.id.Liste_saison);
-        String[] saisons = {"Saison 1", "Saison 2", "Saison 3", "Saison 4"};
-        ArrayAdapter<String> adapter_sai = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, saisons);
-        list_sai.setAdapter(adapter_sai);
-
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
-
     /**
      * ATTENTION: This was auto-generated to implement the App Indexing API.
      * See https://g.co/AppIndexing/AndroidStudio for more information.
@@ -95,21 +90,17 @@ public class MainActivity extends AppCompatActivity {
                 .setActionStatus(Action.STATUS_TYPE_COMPLETED)
                 .build();
     }
-
     @Override
     public void onStart() {
         super.onStart();
-
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
         client.connect();
         AppIndex.AppIndexApi.start(client, getIndexApiAction());
     }
-
     @Override
     public void onStop() {
         super.onStop();
-
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
         AppIndex.AppIndexApi.end(client, getIndexApiAction());
